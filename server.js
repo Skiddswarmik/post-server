9036b72926a461937bf27d54dad2d12facc4ea21
Jack Warr <warrjack@gmail.com>
2026-02-17 23:45:29 -0400

    Enhance CORS support in server.js
    Added CORS headers and OPTIONS method support.

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Allow requests from your GitHub Pages site
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // You can restrict this to your GitHub Pages URL later
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
-app.use(express.json()); 
+app.options('/load', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.use(express.json());
 
 app.post('/load', (req, res) => {
   const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
@@ -23,4 +27,5 @@ app.post('/load', (req, res) => {
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
